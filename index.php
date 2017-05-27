32c25ffda613507eea0df2091ccfac71fa9d4a0c
SULYXXII <SULYXXII@gmail.com>
2017-05-27 14:52:11 +0200

    ohhhh ettttt
    ohhhh ettttt

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ foreach($data as $key=>$value)
       <button class="navbar-toggler navbar-toggler-right hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#">Tableau de bords</a>
+      <a class="navbar-brand" href="#">Tableau de bord</a>
 
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
@@ -65,7 +65,7 @@ foreach($data as $key=>$value)
             <a class="nav-link" href="#">Options</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Profile</a>
+            <a class="nav-link" href="#">Profil</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Aide</a>
@@ -73,7 +73,7 @@ foreach($data as $key=>$value)
         </ul>
         <form class="form-inline mt-2 mt-md-0">
           <input class="form-control mr-sm-2" type="text" placeholder="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Recherche</button>
         </form>
       </div>
     </nav>
