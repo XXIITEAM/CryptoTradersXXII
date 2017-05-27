621de4ef2e9414e1a27dd7c8fd23671f4e5a4d3e
SULYXXII <SULYXXII@gmail.com>
2017-05-27 14:54:46 +0200

    et oh ca va

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ foreach($data as $key=>$value)
       <button class="navbar-toggler navbar-toggler-right hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#">Tableau de bord</a>
+      <a class="navbar-brand" href="#">CryptoTraders XXII</a>
 
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
@@ -72,7 +72,7 @@ foreach($data as $key=>$value)
           </li>
         </ul>
         <form class="form-inline mt-2 mt-md-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search">
+          <input class="form-control mr-sm-2" type="text" placeholder="Saisir une recherche ...">
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Recherche</button>
         </form>
       </div>
